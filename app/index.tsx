fe019582d42e0582a053869f1ce7eda35cf4a9eb
Philipp Jardas <philipp@jardas.de>
2024-10-01 13:11:03 +0200

    Add Firebase

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,44 @@
 import { ThemedText } from "@/components/ThemedText";
+import { getFirestore } from "@react-native-firebase/firestore";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
-import { Button } from "react-native-paper";
+import { ProgressBar } from "react-native-paper";
 
 export default function Index() {
+  const [data, setData] = useState<any>({ state: "loading" });
+
+  useEffect(() =>
+    getFirestore()
+      .collection("users")
+      .doc("WQdXRLTAJAy7WSJ2Z4eoUuhLDYjKKFnTTYNHDOJIysVKo32I")
+      .onSnapshot(
+        (doc) => setData({ state: "ready", data: doc.data() }),
+        (error) => setData({ state: "error", error })
+      )
+  );
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
-        gap: 8,
+        alignItems: "stretch",
+        padding: 16,
       }}
     >
-      <ThemedText>Edit app/index.tsx to edit this screen.</ThemedText>
-      <Button mode="contained" onPress={() => alert("🎉🎉🎉")}>
-        Click Me
-      </Button>
+      {data.state === "loading" && <ProgressBar indeterminate />}
+      {data.state === "error" && <ThemedText>{data.error.message}</ThemedText>}
+      {data.state === "ready" && (
+        <>
+          {data.data ? (
+            <>
+              <ThemedText>{JSON.stringify(data.data, null, 2)}</ThemedText>
+            </>
+          ) : (
+            <ThemedText>Benutzer nicht gefunden.</ThemedText>
+          )}
+        </>
+      )}
     </View>
   );
 }
